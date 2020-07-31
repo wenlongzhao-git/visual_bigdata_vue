aea5921bec1d98f7f257dc57fb196ac4a3bee4e3
赵文龙 <shs-dsjzxzyzwl@chinaunicom.cn>
2020-07-31 17:25:22 +0800

    2020-7-31

---
+++ other_file/jdbc/METAAPPDB.sql
@@ -0,0 +1,16 @@
+/*
+Navicat Oracle Data Transfer
+Oracle Client Version : 10.2.0.5.0
+
+Source Server         : shu_rta
+Source Server Version : 110200
+Source Host           : 134.81.101.5:1521
+Source Schema         : METAAPPDB
+
+Target Server Type    : ORACLE
+Target Server Version : 110200
+File Encoding         : 65001
+
+Date: 2020-07-31 11:12:55
+*/
+
